1fa019df4d98106fd593723cd4913cfa9dc4bd60
Andrew Kovalenko <41870463+andrew899@users.noreply.github.com>
2020-02-27 15:11:07 +0200

    Simple view of processes.
    Menu - new process NOT tested.
    Menu - Refresh DONE
    Menu - Close DONE
    
    Button End Task DONE

---
+++ TaskManager_2020/TaskManager_2020/mainWindow.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace TaskManager_2020
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainWindow_menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.runNewTaskToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Processes_listbox = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.EndTask_btn = new System.Windows.Forms.Button();
+            this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.MainWindow_menuStrip.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainWindow_menuStrip
+            // 
+            this.MainWindow_menuStrip.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.MainWindow_menuStrip.ImageScalingSize = new System.Drawing.Size(28, 28);
+            this.MainWindow_menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.MainWindow_menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.MainWindow_menuStrip.Name = "MainWindow_menuStrip";
+            this.MainWindow_menuStrip.Size = new System.Drawing.Size(800, 38);
+            this.MainWindow_menuStrip.TabIndex = 0;
+            this.MainWindow_menuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.runNewTaskToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.refreshToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(62, 34);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // runNewTaskToolStripMenuItem
+            // 
+            this.runNewTaskToolStripMenuItem.Name = "runNewTaskToolStripMenuItem";
+            this.runNewTaskToolStripMenuItem.Size = new System.Drawing.Size(315, 40);
+            this.runNewTaskToolStripMenuItem.Text = "&Run new Task";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(315, 40);
+            this.closeToolStripMenuItem.Text = "&Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // Processes_listbox
+            // 
+            this.Processes_listbox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Processes_listbox.FormattingEnabled = true;
+            this.Processes_listbox.ItemHeight = 24;
+            this.Processes_listbox.Location = new System.Drawing.Point(13, 42);
+            this.Processes_listbox.Name = "Processes_listbox";
+            this.Processes_listbox.Size = new System.Drawing.Size(775, 268);
+            this.Processes_listbox.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.Controls.Add(this.EndTask_btn);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 322);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 84);
+            this.panel1.TabIndex = 2;
+            // 
+            // EndTask_btn
+            // 
+            this.EndTask_btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.EndTask_btn.Location = new System.Drawing.Point(665, 18);
+            this.EndTask_btn.Name = "EndTask_btn";
+            this.EndTask_btn.Size = new System.Drawing.Size(123, 58);
+            this.EndTask_btn.TabIndex = 0;
+            this.EndTask_btn.Text = "End Task";
+            this.EndTask_btn.UseVisualStyleBackColor = true;
+            this.EndTask_btn.Click += new System.EventHandler(this.EndTask_btn_Click);
+            // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(315, 40);
+            this.refreshToolStripMenuItem.Text = "Refresh";
+            this.refreshToolStripMenuItem.Click += new System.EventHandler(this.refreshToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(312, 6);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(312, 6);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 406);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Processes_listbox);
+            this.Controls.Add(this.MainWindow_menuStrip);
+            this.MainMenuStrip = this.MainWindow_menuStrip;
+            this.Name = "MainWindow";
+            this.Text = "Task Manager";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.MainWindow_menuStrip.ResumeLayout(false);
+            this.MainWindow_menuStrip.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip MainWindow_menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem runNewTaskToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ListBox Processes_listbox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button EndTask_btn;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem refreshToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+    }
+}
+
---
+++ TaskManager_2020/TaskManager_2020/mainWindow.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskManager_2020
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Process[] proc;
+        void GetAllProcess()
+        {
+            proc = Process.GetProcesses();
+
+            Processes_listbox.Items.Clear();
+
+            foreach (Process p in proc)
+            {
+                Processes_listbox.Items.Add(p.ProcessName);
+            }
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            GetAllProcess();
+        }
+
+        private void EndTask_btn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                proc[Processes_listbox.SelectedIndex].Kill();
+                GetAllProcess();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void refreshToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GetAllProcess();
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }       
+    }
+}
